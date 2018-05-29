91fb74c1db7b4c14cef404ab83f8f3834170d2a8
Eugene Yokota <eed3si9n@gmail.com>
2018-05-28 23:18:22 -0400

    cross build to Scala.JS and Scala Native

--- build.sbt
+++ build.sbt
@@ -1,13 +1,21 @@
+// shadow sbt-scalajs' crossProject and CrossType until Scala.js 1.0.0 is released
+import sbtcrossproject.{crossProject, CrossType}
+
 ThisBuild / version := "0.11.0-SNAPSHOT"
 ThisBuild / scalaVersion := "2.12.6"
 ThisBuild / crossScalaVersions := Vector("2.12.6", "2.13.0-M4", "2.11.12", "2.10.6")
 
-lazy val expecty = (project in file("."))
+lazy val root = (project in file("."))
+  .aggregate(expectyJVM, expectyJS)
+  .settings(
+    name := "Expecty Root",
+    Compile / sources := Nil,
+    skip in publish := true,
+  )
+
+lazy val expecty = crossProject(JVMPlatform, JSPlatform, NativePlatform).in(file("."))
   .settings(
     name := "Expecty",
-
-    libraryDependencies += "org.scala-lang" % "scala-reflect" % scalaVersion.value,
-    libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % Test,
     scalacOptions ++= {
       if (scalaVersion.value startsWith "2.10") Nil
       else Seq("-Yrangepos", "-feature", "-deprecation")
@@ -16,4 +24,20 @@ lazy val expecty = (project in file("."))
       if (scalaVersion.value startsWith "2.13") Seq(baseDirectory.value / "src" / "main" / "scala-2.13-beta")
       else Nil
     },
+    libraryDependencies += "org.scala-lang" % "scala-reflect" % scalaVersion.value,
+    libraryDependencies += "com.lihaoyi" %%% "utest" % "0.6.3" % Test,
+    testFrameworks += new TestFramework("utest.runner.Framework"),
   )
+  .jvmSettings(
+    libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % Test,
+  )
+  .jsSettings(
+    scalaJSModuleKind := ModuleKind.CommonJSModule,
+  )
+  .nativeSettings(
+    nativeLinkStubs := true
+  )
+
+lazy val expectyJVM    = expecty.jvm
+lazy val expectyJS     = expecty.js
+lazy val expectyNative = expecty.native
---
+++ shared/src/test/scala/ExpectyUtestSpec.scala
@@ -0,0 +1,67 @@
+/*
+* Copyright 2012 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*     http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package foo
+
+import utest._
+
+object ExpectyUtestSpec extends TestSuite {
+  import com.eed3si9n.expecty.Expecty.assert
+  import java.lang.AssertionError
+
+  val name = "Hi from Expecty!"
+
+  val tests = Tests {
+    "passingExpectation" - {
+      assert(name.length == 16)
+    }
+
+    "failingExpectation" - {
+      intercept[AssertionError] {
+        assert(name.length() == 10)
+      }
+    }
+
+    "multiplePassingExpectations" - {
+      assert(name.length == 16)
+      assert(name.startsWith("Hi"))
+      assert(name.endsWith("Expecty!"))
+    }
+
+    "mixedPassingAndFailingExpectations" - {
+      intercept[AssertionError] {
+        assert(name.length == 16)
+        assert(name.startsWith("Ho"))
+        assert(name.endsWith("Expecty!"))
+      }
+    }
+
+    "passingMultiExpectation" - {
+      assert {
+        name.length == 16
+        name.startsWith("Hi")
+        name.endsWith("Expecty!")
+      }
+    }
+
+    "failingMultiExpectation" - {
+      intercept[AssertionError] {
+        assert {
+          name.length == 16
+          name.startsWith("Ho")
+          name.endsWith("Expecty!")
+        }
+      }
+    }
+  }
+}
